64cb89978a90b5f647954c8dcf74a6eb46cdf18a
Orazio <s300511@studenti.polito.it>
2026-03-17 17:51:14 +0100

    sixth message

--- eval.py
+++ eval.py
@@ -1,7 +1,5 @@
 import dataset.dataset
 import utils.utils
-import torch
-import checkpoints.model
 import train
 
 best_acc = 0
@@ -9,10 +7,10 @@ best_acc = 0
 # Run the training process for {num_epochs} epochs
 num_epochs = 10
 for epoch in range(1, num_epochs + 1):
-    utils.utils.train(epoch, checkpoints.model, dataset.dataset.train_loader, train.criterion, train.optimizer)
+    utils.utils.train(epoch, train.model, dataset.dataset.train_loader, train.criterion, train.optimizer)
 
     # At the end of each training iteration, perform a validation step
-    val_accuracy = utils.utils.validate(checkpoints.model, dataset.dataset.val_loader, train.criterion)
+    val_accuracy = utils.utils.validate(train.model, dataset.dataset.val_loader, train.criterion)
 
     # Best validation accuracy
     best_acc = max(best_acc, val_accuracy)
